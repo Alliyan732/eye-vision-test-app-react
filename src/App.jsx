b49baa31469a2ee426c48c747f4426a46e9c2260
Alliyan <alliyan732@gmail.com>
2023-12-16 22:36:52 +0500

    react icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import "./App.css";
+import React from "react";
+// import {
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider,
+//   Routes,
+// } from "react-router-dom";
+
+import { Fragment, useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+import ColorBlindnessTestScreen from "./pages/ColorBlindTest";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">This is React + Vite + Tailwind Template</h1>;
+  return (
+    <Router>
+      <Route exact path="/" component={ColorBlindnessTestScreen} />
+      <Route path="/about" component={About} />
+    </Router>
+  )
+
+  // <h1 className="text-3xl font-bold underline">This is React + Vite + Tailwind Template</h1>;
 }
 
 export default App;
