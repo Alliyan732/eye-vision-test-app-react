03fa1410d36c81391aca014151832025797cbb27
Alliyan <alliyan732@gmail.com>
2023-12-16 22:56:30 +0500

    astigmatism test added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ColorBlindnessTestScreen from './pages/ColorBlindTest';
 import VisionAcuityTest from './pages/VisionAcuityTest'
 import ContrastSensitivityTestScreen from "./pages/ContrastSensitivityTest";
+import AstigmatismTestScreen from "./pages/AstigmatismTest";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<ColorBlindnessTestScreen />} />
         <Route path="/vision_acuity_test" element={<VisionAcuityTest />} />
         <Route path="/contrast_sensitivity_test" element={<ContrastSensitivityTestScreen />} />
+        <Route path="/astigmatism_test" element={<AstigmatismTestScreen />} />
       </Routes>
     </Router>
   );
