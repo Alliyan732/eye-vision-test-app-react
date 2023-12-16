3d3edcff47eaa2db49e2b2b4cfb0da6ec7576946
Alliyan <alliyan732@gmail.com>
2023-12-16 22:50:03 +0500

    vision acuity test added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import ColorBlindnessTestScreen from "./pages/ColorBlindTest";
+import ColorBlindnessTestScreen from './pages/ColorBlindTest';
+import VisionAcuityTest from './pages/VisionAcuityTest'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ColorBlindnessTestScreen />} />
+        <Route path="/vision_acuity_test" element={<VisionAcuityTest />} />
       </Routes>
     </Router>
   );
